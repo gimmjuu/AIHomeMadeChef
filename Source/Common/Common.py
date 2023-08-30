e6d7aa62da83cecb44efaeb48a8f716ce9ad4e85
gimmjuu <hijklmnoptoz@gmail.com>
2023-08-30 12:45:45 +0900

    230830-혜인-Server 수정

--- Source/Common/Common.py
+++ Source/Common/Common.py
@@ -4,7 +4,8 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QLabel
 
 
-def set_pixmap_from_url(self, t_label: QLabel, t_url: str):
+def set_pixmap_from_url(t_label: QLabel, t_url: str):
+    """QLabel에 Image Url을 setPixmap"""
     # 예시 : urlstring = "https://img.freepik.com/premium-vector/korea-food-menu-set_144089-92.jpg"
     urlstring = t_url
     imgfromurl = urllib.request.urlopen(urlstring).read()
